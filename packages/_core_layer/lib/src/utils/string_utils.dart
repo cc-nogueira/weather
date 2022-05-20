6339af155e76a205bffa86c9fe3c1e9029349b46
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-05-20 15:45:31 -0300

    v0.0.1+5

--- packages/_core_layer/lib/src/utils/string_utils.dart
+++ packages/_core_layer/lib/src/utils/string_utils.dart
@@ -6,4 +6,10 @@ extension StringUtils on String {
     final cut = min(length, max);
     return substring(0, cut);
   }
+
+  String toCapitalized() =>
+      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+
+  String toTitleCase() =>
+      replaceAll(RegExp(' +'), ' ').split(' ').map((str) => str.toCapitalized()).join(' ');
 }
