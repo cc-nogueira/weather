0f2b7fee4329e6ef87c2451a967cb0b8d13b8a17
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-05-23 12:58:16 -0300

    Add today stats to OneCallWeatherStats

--- packages/_presentation_layer/lib/src/feature/weather/one_call_weather/helper/one_call_weather_stats.dart
+++ packages/_presentation_layer/lib/src/feature/weather/one_call_weather/helper/one_call_weather_stats.dart
@@ -4,11 +4,13 @@ import 'package:_domain_layer/domain_layer.dart';
 
 class OneCallWeatherStats {
   OneCallWeatherStats(OneCallWeather oneCallWeather)
-      : hourlyStats = HourlyStats(oneCallWeather),
-        dailyStats = DailyStats(oneCallWeather);
+      : todayStats = TodayStats(oneCallWeather),
+        dailyStats = DailyStats(oneCallWeather),
+        hourlyStats = HourlyStats(oneCallWeather);
 
-  final HourlyStats hourlyStats;
+  final TodayStats todayStats;
   final DailyStats dailyStats;
+  final HourlyStats hourlyStats;
 
   Celcius get minTemp => Celcius(min(hourlyStats._minTemp, dailyStats._minTemp));
   Celcius get maxTemp => Celcius(max(hourlyStats._maxTemp, dailyStats._maxTemp));
@@ -16,15 +18,18 @@ class OneCallWeatherStats {
   bool get hasSnow => hourlyStats.hasSnow || dailyStats.hasSnow;
 }
 
-class HourlyStats {
-  HourlyStats(OneCallWeather oneCallWeather)
-      : hasRain = false,
+abstract class _Stats {
+  _Stats(OneCallWeather oneCallWeather)
+      : hasStats = false,
+        hasRain = false,
         hasSnow = false {
     _calculate(oneCallWeather);
   }
+
   late double _minTemp;
   late double _maxTemp;
 
+  bool hasStats;
   bool hasRain;
   bool hasSnow;
   double maxRain = 0.0;
@@ -33,28 +38,9 @@ class HourlyStats {
   Celcius get minTemp => Celcius(_minTemp);
   Celcius get maxTemp => Celcius(_maxTemp);
 
-  void _calculate(OneCallWeather oneCallWeather) {
-    final seriesData = _seriesData(oneCallWeather);
-    if (seriesData.isEmpty) {
-      _minTemp = 0.0;
-      _maxTemp = 0.0;
-    } else {
-      _minTemp = seriesData.first.conditions.temperatures.temperature.value;
-      _maxTemp = _minTemp;
-      for (final hourly in seriesData) {
-        final conditions = hourly.conditions;
-        _minTemp = min(_minTemp, conditions.temperatures.temperature.value);
-        _maxTemp = max(_maxTemp, conditions.temperatures.temperature.value);
-        maxWind = max(maxWind, hourly.conditions.wind.speed);
-        maxRain = max(maxRain, hourly.conditions.rain1h ?? 0.0);
-        maxSnow = max(maxSnow, hourly.conditions.snow1h ?? 0.0);
-        hasRain = hasRain || (hourly.conditions.rain1h ?? 0.0) > 0.0;
-        hasSnow = hasSnow || (hourly.conditions.snow1h ?? 0.0) > 0.0;
-      }
-    }
-  }
+  void _calculate(OneCallWeather oneCallWeather);
 
-  List<HourlyWeather> _seriesData(OneCallWeather oneCallWeather) {
+  List<HourlyWeather> _hourlySeriesData(OneCallWeather oneCallWeather) {
     final currentHour = _truncatedToHour(DateTime.now()).toUtc();
     final hourly = oneCallWeather.hourly;
     late int i;
@@ -70,31 +56,109 @@ class HourlyStats {
   DateTime _truncatedToHour(DateTime dt) => DateTime(dt.year, dt.month, dt.day, dt.hour);
 }
 
-class DailyStats {
-  DailyStats(OneCallWeather oneCallWeather)
-      : assert(oneCallWeather.daily.isNotEmpty),
-        hasRain = false,
-        hasSnow = false {
-    _calculate(oneCallWeather);
+class TodayStats extends _Stats {
+  TodayStats(super.oneCallWeather);
+
+  @override
+  void _calculate(OneCallWeather oneCallWeather) {
+    final seriesData = _hourlySeriesData(oneCallWeather);
+    if (seriesData.isEmpty && oneCallWeather.daily.isEmpty) {
+      _minTemp = 0.0;
+      _maxTemp = 0.0;
+      hasStats = false;
+      return;
+    }
+    final today = todayDay(oneCallWeather.weather.geo);
+    if (oneCallWeather.daily.isNotEmpty) {
+      if (oneCallWeather.daily.first.localShiftedDateTime.day == today) {
+        hasStats = true;
+        final conditions = oneCallWeather.daily.first.conditions;
+        _minTemp = conditions.dailyMin.value;
+        _maxTemp = conditions.dailyMax.value;
+        hasRain = hasRain || (conditions.rain ?? 0.0) > 0.0;
+        hasSnow = hasSnow || (conditions.snow ?? 0.0) > 0.0;
+      }
+    }
+    if (seriesData.isNotEmpty) {
+      if (!hasStats && seriesData.first.localShiftedDateTime.day == today) {
+        hasStats = true;
+        final conditions = seriesData.first.conditions;
+        _minTemp = conditions.temperatures.temperature.value;
+        _maxTemp = _minTemp;
+        hasRain = hasRain || (conditions.rain1h ?? 0.0) > 0.0;
+        hasSnow = hasSnow || (conditions.snow1h ?? 0.0) > 0.0;
+      }
+      for (final hourly in seriesData) {
+        if (hourly.localShiftedDateTime.day != today) {
+          break;
+        }
+        final conditions = hourly.conditions;
+        _minTemp = min(_minTemp, conditions.temperatures.temperature.value);
+        _maxTemp = max(_maxTemp, conditions.temperatures.temperature.value);
+        maxWind = max(maxWind, conditions.wind.speed);
+        maxRain = max(maxRain, conditions.rain1h ?? 0.0);
+        maxSnow = max(maxSnow, conditions.snow1h ?? 0.0);
+        hasRain = hasRain || (conditions.rain1h ?? 0.0) > 0.0;
+        hasSnow = hasSnow || (conditions.snow1h ?? 0.0) > 0.0;
+      }
+    }
   }
 
-  late double _minTemp;
-  late double _maxTemp;
+  int todayDay(Geo geo) {
+    final now = DateTime.now().toUtc();
+    final shifted = DateTime.fromMillisecondsSinceEpoch(
+        now.millisecondsSinceEpoch + geo.timeShiftMillis,
+        isUtc: true);
+    return DateTime(shifted.year, shifted.month, shifted.day, shifted.hour, shifted.minute,
+            shifted.second, shifted.millisecond, shifted.microsecond)
+        .day;
+  }
+}
 
-  bool hasRain;
-  bool hasSnow;
-  Celcius get minTemp => Celcius(_minTemp);
-  Celcius get maxTemp => Celcius(_maxTemp);
+class DailyStats extends _Stats {
+  DailyStats(super.oneCallWeather);
 
+  @override
   void _calculate(OneCallWeather oneCallWeather) {
-    _minTemp = oneCallWeather.daily.first.conditions.dailyMin.value;
-    _maxTemp = oneCallWeather.daily.first.conditions.dailyMax.value;
-    for (final daily in oneCallWeather.daily) {
-      final conditions = daily.conditions;
-      _minTemp = min(_minTemp, conditions.dailyMin.value);
-      _maxTemp = max(_maxTemp, conditions.dailyMax.value);
-      hasRain = hasRain || (daily.conditions.rain ?? 0.0) > 0.0;
-      hasSnow = hasSnow || (daily.conditions.snow ?? 0.0) > 0.0;
+    if (oneCallWeather.daily.isEmpty) {
+      _minTemp = 0.0;
+      _maxTemp = 0.0;
+    } else {
+      _minTemp = oneCallWeather.daily.first.conditions.dailyMin.value;
+      _maxTemp = oneCallWeather.daily.first.conditions.dailyMax.value;
+      for (final daily in oneCallWeather.daily) {
+        final conditions = daily.conditions;
+        _minTemp = min(_minTemp, conditions.dailyMin.value);
+        _maxTemp = max(_maxTemp, conditions.dailyMax.value);
+        hasRain = hasRain || (daily.conditions.rain ?? 0.0) > 0.0;
+        hasSnow = hasSnow || (daily.conditions.snow ?? 0.0) > 0.0;
+      }
+    }
+  }
+}
+
+class HourlyStats extends _Stats {
+  HourlyStats(super.oneCallWeather);
+
+  @override
+  void _calculate(OneCallWeather oneCallWeather) {
+    final seriesData = _hourlySeriesData(oneCallWeather);
+    if (seriesData.isEmpty) {
+      _minTemp = 0.0;
+      _maxTemp = 0.0;
+    } else {
+      _minTemp = seriesData.first.conditions.temperatures.temperature.value;
+      _maxTemp = _minTemp;
+      for (final hourly in seriesData) {
+        final conditions = hourly.conditions;
+        _minTemp = min(_minTemp, conditions.temperatures.temperature.value);
+        _maxTemp = max(_maxTemp, conditions.temperatures.temperature.value);
+        maxWind = max(maxWind, conditions.wind.speed);
+        maxRain = max(maxRain, conditions.rain1h ?? 0.0);
+        maxSnow = max(maxSnow, conditions.snow1h ?? 0.0);
+        hasRain = hasRain || (conditions.rain1h ?? 0.0) > 0.0;
+        hasSnow = hasSnow || (conditions.snow1h ?? 0.0) > 0.0;
+      }
     }
   }
 }
