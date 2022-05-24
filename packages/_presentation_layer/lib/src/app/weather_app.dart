6a7575a582911aecda1fad991e197dd88943b167
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-05-24 16:18:40 -0300

    Using localized country from project fork

--- packages/_presentation_layer/lib/src/app/weather_app.dart
+++ packages/_presentation_layer/lib/src/app/weather_app.dart
@@ -1,6 +1,6 @@
 import 'package:_domain_layer/domain_layer.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 
@@ -34,28 +34,29 @@ class WeatherApp extends ConsumerWidget with WidgetsBindingObserver {
     return error == null ? _app(ref) : _errorApp;
   }
 
-  Widget _app(WidgetRef ref) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ref.watch(themeProvider),
-        onGenerateTitle: (context) => Translations.of(context)!.weather_list_page_title,
-        localizationsDelegates: const [
-          ...Translations.localizationsDelegates,
-          SfGlobalLocalizations.delegate
-        ],
-        supportedLocales: Translations.supportedLocales,
-        locale: ref.watch(languageOptionProvider).locale,
-        onGenerateRoute: _routes.onGenerateRoute,
-        initialRoute: Routes.home,
-      );
+  Widget _app(WidgetRef ref) {
+    final locale = ref.watch(languageOptionProvider).locale;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ref.watch(themeProvider),
+      onGenerateTitle: (context) => Translations.of(context)!.weather_list_page_title,
+      localizationsDelegates: const [
+        CountryLocalizations.delegate,
+        SfGlobalLocalizations.delegate,
+        ...Translations.localizationsDelegates,
+      ],
+      supportedLocales: Translations.supportedLocales,
+      locale: locale,
+      onGenerateRoute: _routes.onGenerateRoute,
+      initialRoute: Routes.home,
+    );
+  }
 
   Widget get _errorApp => MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.blue),
         onGenerateTitle: (context) => Translations.of(context)!.weather_list_page_title,
-        localizationsDelegates: const [
-          Translations.delegate,
-          ...GlobalMaterialLocalizations.delegates,
-        ],
+        localizationsDelegates: Translations.localizationsDelegates,
         supportedLocales: Translations.supportedLocales,
         home: ErrorMessagePage(error!),
       );
