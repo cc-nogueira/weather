bb9465046ba6a61b42e564c83d42c6689e718e0f
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-05-27 17:44:22 -0300

    Tip for drawer close button

--- packages/_presentation_layer/lib/src/feature/settings/widget/open_end_drawer_button.dart
+++ packages/_presentation_layer/lib/src/feature/settings/widget/open_end_drawer_button.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../l10n/translations.dart';
+
 class OpenEndDrawerButton extends StatelessWidget {
   const OpenEndDrawerButton({super.key});
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.only(right: 4.0),
-        child: IconButton(
-          icon: const Icon(Icons.settings),
-          onPressed: () => Scaffold.of(context).openEndDrawer(),
-        ),
-      );
+  Widget build(BuildContext context) {
+    final translations = Translations.of(context)!;
+    return Padding(
+      padding: const EdgeInsets.only(right: 4.0),
+      child: IconButton(
+        icon: const Icon(Icons.settings),
+        tooltip: translations.title_preferences,
+        onPressed: () => Scaffold.of(context).openEndDrawer(),
+      ),
+    );
+  }
 }
