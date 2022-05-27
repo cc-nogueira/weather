7e145d18627cc257159764bb0891019b3c849890
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-05-27 17:44:02 -0300

    Better colors at AppBar and Preferences Drawer

--- packages/_presentation_layer/lib/src/common/helper/theme_builder.dart
+++ packages/_presentation_layer/lib/src/common/helper/theme_builder.dart
@@ -10,10 +10,14 @@ class ThemeBuilder {
     final base = mode == ThemeMode.dark
         ? ThemeData(brightness: Brightness.dark, useMaterial3: true)
         : ThemeData(brightness: Brightness.light, useMaterial3: true);
+
+    final bgColor = _scaffoldBackgroundColor(mode);
+
     return base.copyWith(
       colorScheme: _colorScheme(mode, base.colorScheme),
       textTheme: _textTheme(mode, base.textTheme),
-      scaffoldBackgroundColor: _scaffoldBackgroundColor(mode),
+      appBarTheme: _appBarTheme(mode, base.appBarTheme),
+      scaffoldBackgroundColor: bgColor,
     );
   }
 
@@ -22,6 +26,9 @@ class ThemeBuilder {
 
   TextTheme _textTheme(ThemeMode mode, TextTheme base) =>
       mode == ThemeMode.dark ? base : base.apply(bodyColor: Colors.brown[900]);
+
+  AppBarTheme _appBarTheme(ThemeMode mode, AppBarTheme base) =>
+      base.copyWith(backgroundColor: _scaffoldBackgroundColor(mode));
 
   Color _scaffoldBackgroundColor(ThemeMode mode) =>
       mode == ThemeMode.dark ? Colors.grey[900]! : const Color(0xFFCACAC0);
