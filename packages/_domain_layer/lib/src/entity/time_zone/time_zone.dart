ce7a0148b6fb2870053a948bd36e6c69159c5313
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-06-02 10:15:35 -0300

    Documentation of time zone entities

--- packages/_domain_layer/lib/src/entity/time_zone/time_zone.dart
+++ packages/_domain_layer/lib/src/entity/time_zone/time_zone.dart
@@ -2,6 +2,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'time_zone.freezed.dart';
 
+/// TimeZone entity.
+///
+/// Freezed class with time zone information.
+/// It also provides a getter to convert int millis to currentLocalTime.
 @freezed
 class TimeZone with _$TimeZone {
   const TimeZone._();
@@ -21,6 +25,7 @@ class TimeZone with _$TimeZone {
   Duration get currentUtcOffsetDuration => Duration(seconds: currentUtcOffset.seconds);
 }
 
+/// TimeZoneOffset entity used by TimeZone.
 @freezed
 class TimeZoneOffset with _$TimeZoneOffset {
   const factory TimeZoneOffset({
@@ -29,6 +34,7 @@ class TimeZoneOffset with _$TimeZoneOffset {
   }) = _TimeZoneOffset;
 }
 
+/// DstInterval entity used by TimeZone.
 @freezed
 class DstInterval with _$DstInterval {
   const factory DstInterval({
