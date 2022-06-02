995ee7567da71702e2db7accc4732d3efdae0e03
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-06-02 11:04:47 -0300

    Documentation of the DomainLayer object.

--- packages/_domain_layer/lib/src/layer/domain_layer.dart
+++ packages/_domain_layer/lib/src/layer/domain_layer.dart
@@ -1,5 +1,6 @@
 import 'package:_core_layer/core_layer.dart';
 import 'package:logging/logging.dart';
+import 'package:meta/meta.dart';
 import 'package:riverpod/riverpod.dart';
 
 import '../repository/cities_repository.dart';
@@ -11,7 +12,10 @@ import '../usecase/preferences_usecase.dart';
 import '../usecase/time_zone_usecase.dart';
 import '../usecase/weather_usecase.dart';
 
-/// Function definition for Domain Layer dependencies
+/// Function definition for Domain Layer dependencies.
+///
+/// Defines the required implementations of known interfaces.
+/// This signature is used by the [configure] method bellow to inject dependencies.
 typedef DomainConfiguration = void Function({
   required PreferencesRepository preferencesRepository,
   required CitiesRepository citiesRepository,
@@ -28,17 +32,35 @@ typedef DomainConfiguration = void Function({
 /// DomainLayer configuration is also available through [domainConfigurationProvider].
 ///
 /// Domains usecases are available through usecase providers:
+///   - [preferencesUsecase]
 ///   - [citiesUsecase]
+///   - [timeUsecase]
 ///   - [weatherUsecase]
 class DomainLayer extends AppLayer {
+  /// Constructor.
+  ///
+  /// Required a Riverpod Reader to instantite the [PreferencesUsecase].
   DomainLayer({required this.read});
 
+  /// Internal reader
+  @internal
   final Reader read;
+
+  /// Configured [PreferencesUsecase] singleton.
   late final PreferencesUsecase preferencesUsecase;
+
+  /// Configured [CitiesUsecase] singleton.
   late final CitiesUsecase citiesUsecase;
+
+  /// Configured [TimeZoneUsecase] singleton.
   late final TimeZoneUsecase timeUsecase;
+
+  /// Configured [WeatherUsecase] singleton.
   late final WeatherUsecase weatherUsecase;
 
+  /// Configure method called by the [DiLayer] object at system initialization.
+  ///
+  /// This invocation injects the required implementations for this Layer.
   void configure({
     required PreferencesRepository preferencesRepository,
     required CitiesRepository citiesRepository,
