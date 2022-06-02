3ff3488791a8000d2e97a63519d17fb71745879c
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-06-02 14:43:06 -0300

    Documentation for domain providers.

--- packages/_domain_layer/lib/src/provider/providers.dart
+++ packages/_domain_layer/lib/src/provider/providers.dart
@@ -25,6 +25,7 @@ final domainConfigurationProvider =
 
 // -- AppLifecycle:
 
+/// AppLifecycleUsecase singleton provider
 final appLifecycleUsecaseProvider =
     Provider<AppLifecycleUsecase>((ref) => AppLifecycleUsecase(ref: ref));
 
@@ -33,27 +34,34 @@ final appLifecycleUsecaseProvider =
 /// System locales obtained on main() and updated by PresentationLayer App.
 final systemLocalesProvider = StateProvider<List<Locale>>((ref) => []);
 
+/// PreferencesUsecase singleton provider
 final preferencesUsecaseProvider =
     Provider<PreferencesUsecase>((ref) => ref.watch(domainLayerProvider).preferencesUsecase);
 
 // -- Cities:
 
+/// CitiesUsecase singleton provider
 final citiesUsecaseProvider =
     Provider<CitiesUsecase>((ref) => ref.watch(domainLayerProvider).citiesUsecase);
 
+/// Provider for city search service accessed through the usecase.
 final citiesSearchProvider = FutureProvider.autoDispose.family<List<City>, City>(
   (ref, city) => ref.watch(
     weatherUsecaseProvider.select((usecase) => usecase.searchCitiesLike(city)),
   ),
 );
 
+/// StreamProvider to watch the live list of registered cities.
+/// This providers is hooked to the City repository state.
 final watchAllCitiesProvider = StreamProvider((ref) => ref.watch(citiesUsecaseProvider).watchAll());
 
 // -- Time Zone:
 
+/// TimeZoneUsecase singleton provider
 final timeUsecaseProvider =
     Provider<TimeZoneUsecase>((ref) => ref.watch(domainLayerProvider).timeUsecase);
 
+/// Provider for a location timezone service accessed through the usecase.
 final timeZoneProvider = FutureProvider.family<TimeZone, Location>(
   (ref, location) {
     ref.watch(currentWeatherMetronomeProvider);
@@ -65,21 +73,32 @@ final timeZoneProvider = FutureProvider.family<TimeZone, Location>(
 
 // -- Weather:
 
+/// WeatherUsecase singleton provider
 final weatherUsecaseProvider =
     Provider<WeatherUsecase>((ref) => ref.watch(domainLayerProvider).weatherUsecase);
 
+/// CurrentWeather provider for a location.
+/// This provider auto refreshs on currentWeatherMetronomeProvider.
 final currentWeatherByLocationAutoRefreshProvider =
     FutureProvider.family<CurrentWeather, Location>((ref, location) {
   ref.watch(currentWeatherMetronomeProvider);
   return ref.watch(weatherUsecaseProvider).getCurrentWeatherByLocation(location);
 });
 
+/// OneCallWeather tuple provider for a location.
+/// Returns the current weather and a timestamp.
+/// This provider is invalidated manually by a RefreshButton or RefreshIndicator.
+/// The timestamp is avalilable to permit frequent refresh prevention.
 final oneCallWeatherTupleByLocationProvider =
     FutureProvider.family<Tuple2<OneCallWeather, DateTime>, Location>((ref, location) async {
   final weather = await ref.watch(weatherUsecaseProvider).getOneCallByLocation(location);
   return Tuple2(weather, DateTime.now());
 });
 
+/// OneCallWeather auto evict provider for a location.
+/// This autodispose provider relies on OneCallWeatherTuple provider for the caching, verifying
+/// the refresh interval defined in the Usecase class.
+/// It invalidates the referenced provider when necessary (time elapsed > refreshInterval).
 final oneCallWeatherByLocationAutoEvictProvider =
     FutureProvider.autoDispose.family<OneCallWeather, Location>((ref, location) async {
   final tuple = await ref.read(oneCallWeatherTupleByLocationProvider(location).future);
@@ -93,18 +112,33 @@ final oneCallWeatherByLocationAutoEvictProvider =
 
 // -- Metronome:
 
+/// minuteMetronome provider subscribes to a Metronome with aMinute interval.
+/// This provider is invalidated by AppLifecycleUsecase when App is put in background.
+/// It will resume execution when observed next time by the resumed user interface.
+///
+/// This provider is used to show up to date local time for all locations.
 final minuteMetronomeProvider = Provider<DateTime>((ref) {
   final subscription = Metronome.epoch(aMinute).listen((dt) => ref.state = dt);
   ref.onDispose(() => subscription.cancel());
   return DateTime.now();
 });
 
+/// hourMetronome provider subscribes to a Metronome with anHour interval.
+/// This provider is invalidated by AppLifecycleUsecase when App is put in background.
+/// It will resume execution when observed next time by the resumed user interface.
+///
+/// This provider is used to refresh chart information that is hour based.
 final hourMetronomeProvider = Provider<DateTime>((ref) {
   final subscription = Metronome.epoch(anHour).listen((dt) => ref.state = dt);
   ref.onDispose(() => subscription.cancel());
   return DateTime.now();
 });
 
+/// currentWeatherMetronome provider subscrive to a Metronome with the interval defined in the Usecase.
+/// This provider is invalidated by AppLifecycleUsecase when App is put in background.
+/// It will resume execution when observed next time by the resumed user interface.
+///
+/// This provider is used to refresh current weather for all locations..
 final currentWeatherMetronomeProvider = Provider<DateTime>((ref) {
   ref.watch(languageOptionProvider);
   final subscription = Metronome.periodic(WeatherUsecase.currentWeatherRefreshInterval)
