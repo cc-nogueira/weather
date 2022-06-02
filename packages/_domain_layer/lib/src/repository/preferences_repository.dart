6c904b9b8fe112758c68d85165261db61b1d68cf
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-06-02 14:53:24 -0300

    Documentation for Repository interfaces.

--- packages/_domain_layer/lib/src/repository/preferences_repository.dart
+++ packages/_domain_layer/lib/src/repository/preferences_repository.dart
@@ -1,7 +1,18 @@
 import '../entity/common/preference.dart';
-import 'entity_repository.dart';
 
-abstract class PreferencesRepository implements EntityRepository<Preference> {
+/// PreferencesRepository interface.
+///
+/// Very simple Repository interface useing only an API that reads and saves by a String key.
+abstract class PreferencesRepository {
+  /// Get a preference by key.
+  ///
+  /// Look for a [Preference] by a key.
+  /// Return null if none is found.
   Preference? getByKey(String key);
+
+  /// Save a preference by key.
+  ///
+  /// Saves a [Preference] by its internal key.
+  /// Returns the saved entity (may have an assigned id if it was new).
   Preference saveByKey(Preference preference);
 }
