d3c0771e5177c125fae9175aa9539d0530b1974b
ricardo-nogueira-cc <ricardo@nogueira.cc>
2022-06-03 13:58:25 -0300

    PresentationLayer documentation and code review.

--- packages/_presentation_layer/lib/src/common/mobile_add/weather_ad_unit_ids.dart
+++ packages/_presentation_layer/lib/src/common/mobile_add/weather_ad_unit_ids.dart
@@ -1,10 +1,26 @@
 import 'ad_unit_ids.dart';
 
+/// Application AdUnitIds for Android.
+///
+/// Registered IDs for Android platform
+///
+/// This file should not be persisted with official IDs in public repositories.
+/// It is recommended to either exclude in .gitignore or to save it first with emptyIds and then
+/// exclude it from further changes with:
+///   > git update-index --skip-worktree <path-name>
 class WeatherAndroidAdUnitIds extends AndroidAdUnitIds {
   // TODO: Set your bannerId for Android
   const WeatherAndroidAdUnitIds() : super(bannerId: '');
 }
 
+/// Application AdUnitIds for IOS.
+///
+/// Registered IDs for IOS platform
+///
+/// This file should not be persisted with official IDs in public repositories.
+/// It is recommended to either exclude in .gitignore or to save it first with emptyIds and then
+/// exclude it from further changes with:
+///   > git update-index --skip-worktree <path-name>
 class WeatherIosAdUnitIds extends AndroidAdUnitIds {
   // TODO: Set your bannerId for IOS
   const WeatherIosAdUnitIds() : super(bannerId: '');
